5aee1d25cb588415a313cc23b32033ed49115e78
arifraj199 <karif9514@gmail.com>
2024-04-28 15:42:07 +0600

    tourist spot card design implement

--- src/Pages/SingleSpotCard/SingleSpotCard.jsx
+++ src/Pages/SingleSpotCard/SingleSpotCard.jsx
@@ -1,7 +1,13 @@
 import PropTypes from "prop-types";
+import { CiLocationOn } from "react-icons/ci";
+import { GiDuration } from "react-icons/gi";
+import { LuUser2 } from "react-icons/lu";
+import { PiSunHorizonLight } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 const SingleSpotCard = ({ spots }) => {
   const {
+    _id,
     spot_name,
     image,
     country_name,
@@ -13,22 +19,53 @@ const SingleSpotCard = ({ spots }) => {
     description,
   } = spots;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 flex flex-col relative shadow-xl">
       <figure className="h-[300px]">
         <img className="w-full h-full" src={image} alt="Shoes" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {spot_name}
-          <div className="badge badge-secondary">{travel_time}</div>
+      <span className="absolute bg-[#C40C0C] font-light text-white rounded-lg left-4 top-4 px-3 py-2">
+        {country_name}
+      </span>
+      <div className="card-body flex-grow p-5">
+        <div className="flex items-center gap-1  text-lg font-semibold text-[#999]">
+          <CiLocationOn />
+          <span>{location}</span>
+        </div>
+        <h2 className="items-center flex justify-between text-2xl">
+          <span className="font-bold">{spot_name}</span>
+          {/* <div className="badge badge-secondary">{travel_time}</div> */}
+          <div className="text-xl font-normal">
+            <span className="text-[#C40C0C]">${avg_cost}</span> /avg
+          </div>
         </h2>
-        <p>Country:{country_name}</p>
-        <p>Location:{location}</p>
-        <p>Seasonality:{season}</p>
-        <p>{description}</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Avg_cost:{avg_cost}</div>
-          <div className="badge badge-outline">Visitor:{visitor}</div>
+
+        <p className="mt-6 font-semibold text-[#999]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis illo
+          accusamus nobis.{" "}
+        </p>
+        <hr className="my-4" />
+        <div className="flex items-center justify-between font-semibold text-[#999]">
+          <div className="flex items-center gap-2">
+            <LuUser2 />
+            <span>{visitor}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <GiDuration />
+            <span>{travel_time}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <PiSunHorizonLight />
+            <span>{season}</span>
+          </div>
+        </div>
+        <hr className="my-4" />
+        <div className=" flex justify-end">
+          <Link
+            to={`/all-tourist-spot/${_id}`}
+            className="bg-[#C40C0C] text-white px-5 py-3 font-normal rounded-lg text-lg"
+          >
+            View Details
+          </Link>
         </div>
       </div>
     </div>
--- src/Pages/TouristSpots/TouristSpots.jsx
+++ src/Pages/TouristSpots/TouristSpots.jsx
@@ -5,8 +5,13 @@ const TouristSpots = ({ spotData }) => {
   // console.log(spotData);
   return (
     <div className="my-20 max-w-screen-xl mx-auto flex flex-col items-center">
-      <h2 className="text-2xl my-20 ">View Our Spots</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <h2 className="text-5xl mt-24 mb-5 font-bold ">Most Popular Tours</h2>
+      <p className="w-3/5 mx-auto text-center text-[#999]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        praesentium impedit nostrum fuga distinctio a accusamus repellat enim
+        veritatis consequuntur.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-20">
         {spotData.slice(0, 6).map((spots) => (
           <SingleSpotCard key={spots._id} spots={spots}></SingleSpotCard>
         ))}
