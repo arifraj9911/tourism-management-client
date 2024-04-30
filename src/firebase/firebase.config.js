bd8824e317ab5851673acd95d743e6808fad96cb
arifraj199 <karif9514@gmail.com>
2024-04-30 18:00:40 +0600

    firebase environment variable setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMmH1o4-lfUcv31BlhGvMrhWn2Wo45LHo",
-  authDomain: "tourism-management-69cfd.firebaseapp.com",
-  projectId: "tourism-management-69cfd",
-  storageBucket: "tourism-management-69cfd.appspot.com",
-  messagingSenderId: "816828613085",
-  appId: "1:816828613085:web:9a8676ec292a648c513634"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
